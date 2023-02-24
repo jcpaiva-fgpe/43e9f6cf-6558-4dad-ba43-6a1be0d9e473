f757b24a8939e2dfb9810adb666e9a9f14a0e1dc
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-02-24 11:43:33 +0000

    updated file exercises/5b63245d-cc20-47e1-b3d9-5c453597fb0f/solutions/d956125b-a99e-4c25-87a0-662a417d6ee4/solution.sql

--- exercises/5b63245d-cc20-47e1-b3d9-5c453597fb0f/solutions/d956125b-a99e-4c25-87a0-662a417d6ee4/solution.sql
+++ exercises/5b63245d-cc20-47e1-b3d9-5c453597fb0f/solutions/d956125b-a99e-4c25-87a0-662a417d6ee4/solution.sql
@@ -1,3 +1,2 @@
-CREATE TYPE activoType AS ENUM ('SI', 'NO');
 ALTER TABLE equipo ADD director varchar(30) NULL;
-ALTER TABLE equipo ADD activo activoType NULL;
+ALTER TABLE equipo ADD activo CHAR(2) null check (activo in ('SI', 'NO'));
