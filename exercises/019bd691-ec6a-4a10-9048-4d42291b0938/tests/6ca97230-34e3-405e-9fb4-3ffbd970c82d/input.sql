4d7a9c172561339d266bb0b0c2cdb3119cdbcf17
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-02-24 11:42:54 +0000

    updated file exercises/019bd691-ec6a-4a10-9048-4d42291b0938/tests/6ca97230-34e3-405e-9fb4-3ffbd970c82d/input.sql

--- exercises/019bd691-ec6a-4a10-9048-4d42291b0938/tests/6ca97230-34e3-405e-9fb4-3ffbd970c82d/input.sql
+++ exercises/019bd691-ec6a-4a10-9048-4d42291b0938/tests/6ca97230-34e3-405e-9fb4-3ffbd970c82d/input.sql
@@ -1,7 +1,7 @@
 INSERT INTO ciclista (ficha,fecha_nac,nombre)
 	VALUES (1,'2001-10-02','Pepe');
-INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof)
-	VALUES (2,'2000-01-12','Juana','Francia',2019,2018);
+INSERT INTO ciclista (ficha,fecha_nac,nombre,anyo_retiro,anyo_prof)
+	VALUES (2,'2000-01-12','Juana',2019,2018);
 INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof)
 	VALUES (3,'1999-03-22','Juan','Italia',NULL,2020);
 INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof, pertenece)
