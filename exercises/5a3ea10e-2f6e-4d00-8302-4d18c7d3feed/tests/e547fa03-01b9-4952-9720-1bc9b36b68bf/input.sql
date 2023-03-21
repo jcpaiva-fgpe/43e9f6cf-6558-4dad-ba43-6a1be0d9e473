4f3daf43e87a171792dbf69aa8b041f5cfb2f6b4
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-03-21 11:21:10 +0000

    updated file exercises/5a3ea10e-2f6e-4d00-8302-4d18c7d3feed/tests/e547fa03-01b9-4952-9720-1bc9b36b68bf/input.sql

--- exercises/5a3ea10e-2f6e-4d00-8302-4d18c7d3feed/tests/e547fa03-01b9-4952-9720-1bc9b36b68bf/input.sql
+++ exercises/5a3ea10e-2f6e-4d00-8302-4d18c7d3feed/tests/e547fa03-01b9-4952-9720-1bc9b36b68bf/input.sql
@@ -1,43 +1,22 @@
-INSERT INTO ciclista (ficha,fecha_nac,nombre)
-	VALUES (1,'2001-10-02','Pepe');
-INSERT INTO ciclista (ficha,fecha_nac,nombre,anyo_retiro,anyo_prof)
-	VALUES (2,'2000-01-12','Juana',2019,2018);
-INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof)
-	VALUES (3,'1999-03-22','Juan','Italia',NULL,2020);
-INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof, pertenece)
-	VALUES (4,'1999-03-22','Juan','Italia',NULL,2020, 2018);
-INSERT INTO equipo (nombre,anyo_apar)
-	VALUES ('Itinere',2022);
-INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof, equipo)
-	VALUES (5,'1999-03-22','Juan','Italia',NULL,2020, 2018, 'Itinere');
-INSERT INTO equipo (nombre,anyo_apar,anyo_des)
-	VALUES ('Encuentro',2018,2021);
-INSERT INTO equipo (nombre,anyo_apar)
-	VALUES ('ImaginaUnaEmpresaDiferenteImaginaUnaEmpresaDiferenteImaginaUnaEmpresaDiferenteImaginaUnaEmpresaDiferente',2019);
-INSERT INTO equipo (nombre,anyo_apar)
-	VALUES ('InnovaFest',2021);
-INSERT INTO equipo (nombre)
-	VALUES ('ImaginaUnaEmpresaDiferente');
-INSERT INTO equipo (nombre,anyo_apar)
-	VALUES ('InnovaFest',2022);
-INSERT INTO pertenece (ficha,nombre)
-	VALUES (1,'Itinere');
-INSERT INTO pertenece (ficha,nombre,anyo)
-	VALUES (2,'Itineres',2022);
-INSERT INTO pertenece (ficha,nombre,anyo)
-	VALUES (21,'Itinere',2022);
-INSERT INTO pertenece (ficha,nombre,anyo)
-	VALUES (3,'Itinere',2022);
-INSERT INTO pertenece (ficha,nombre,anyo)
-	VALUES (3,'Itinere',2022);
-INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof, pertenece)
-	VALUES (6,'1999-03-22','Juan','Italia',NULL,2020, 2018);
-INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof, equipo)
-	VALUES (7,'1999-03-22','Juan','Italia',NULL,2020, 'Itinere');
+INSERT INTO partidos (cPartido,siglas,denominacion,votos,escanyos)
+	VALUES (1000,'c3','CIFP Carlos III',1000,2);
+INSERT INTO partidos (cPartido,siglas,denominacion,votos,escanyos)
+	VALUES (150001,null, null,1000,2);
+INSERT INTO partidos (cPartido,siglas,denominacion,votos,escanyos)
+	VALUES (150001,'c3','CIFP Carlos III',null,null);
+INSERT INTO partidos (cPartido,siglas,denominacion,votos,escanyos)
+	VALUES (150001,'c3','CIFP Carlos III',1000,2);
+INSERT INTO partidos (cPartido,siglas,denominacion,votos,escanyos)
+	VALUES (150002,'PS','Partido de las soluciones',500,1);
+INSERT INTO candidato (dni,nombre,apellidos,cpartido)
+	VALUES ('12345678A','Pepe','García',150000);
+INSERT INTO candidato (dni,nombre,apellidos,cpartido)
+	VALUES ('12345678B',null,null,150001);
+INSERT INTO candidato (dni,nombre,apellidos,cpartido)
+	VALUES ('12345678C','Pepa','García',150001);
+INSERT INTO candidato (dni,nombre,apellidos,posicionLista,cpartido)
+	VALUES ('12345678D','Juan','Fernández',2,150001);
 
-select ficha, pais, anyo_prof  from CICLISTA
+select cPartido,siglas,denominacion,escanyos from partidos
 union
-select anyo_apar, nombre, anyo_des from equipo
-union
-select ficha , nombre, anyo from pertenece
-order by 1,2;
+select cpartido, dni,nombre,posicionLista from candidato;
