ab1a184c52be6666bb0658a109e4af33aabb4807
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-03-22 09:41:43 +0000

    updated file exercises/5458c09d-e2df-417d-88fd-d844630c1f6f/tests/2c455586-b1f8-4de0-9332-a2ac02137d6c/input2.sql

--- exercises/5458c09d-e2df-417d-88fd-d844630c1f6f/tests/2c455586-b1f8-4de0-9332-a2ac02137d6c/input2.sql
+++ exercises/5458c09d-e2df-417d-88fd-d844630c1f6f/tests/2c455586-b1f8-4de0-9332-a2ac02137d6c/input2.sql
@@ -1,5 +1,5 @@
 INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
 	 ('101','Inserción Válida','1000','2','800','20','6','710');
 INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
-	 ('102','Inserción inválida','1000','2','800','10','5','789');
+	 ('102','Inserción inválida','1000','2','800','10','5','788');
 select * from municipios m where cast(cmunicipio as integer) > 100 order by cmunicipio;
