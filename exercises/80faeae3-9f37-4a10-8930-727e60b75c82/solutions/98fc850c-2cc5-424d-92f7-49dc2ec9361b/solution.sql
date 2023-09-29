afa925e52056024cbff7f42d23799650fc4e3f0e
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-09-29 11:21:12 +0100

    updated file exercises/80faeae3-9f37-4a10-8930-727e60b75c82/solutions/98fc850c-2cc5-424d-92f7-49dc2ec9361b/solution.sql

--- exercises/80faeae3-9f37-4a10-8930-727e60b75c82/solutions/98fc850c-2cc5-424d-92f7-49dc2ec9361b/solution.sql
+++ exercises/80faeae3-9f37-4a10-8930-727e60b75c82/solutions/98fc850c-2cc5-424d-92f7-49dc2ec9361b/solution.sql
@@ -1,2 +1,3 @@
-SELECT DISTINCT JOB_ID FROM employees
-order by job_id;
+select first_name, last_name, job_id from employees
+where salary between 10000 and 18000 and commission_pct is null
+hr-> order by job_id, last_name;
