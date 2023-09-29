59c0c3d17687fe155ed49e219b4200c742553eff
Alberto Sierra <alberto.sierra@murciaeduca.es>
2023-09-29 11:07:58 +0100

    updated file exercises/bd9dafb5-b468-487f-8b42-37060613e8df/solutions/f6030c22-b53e-4d9f-8c9e-a4aa110c412e/solution.sql

--- exercises/bd9dafb5-b468-487f-8b42-37060613e8df/solutions/f6030c22-b53e-4d9f-8c9e-a4aa110c412e/solution.sql
+++ exercises/bd9dafb5-b468-487f-8b42-37060613e8df/solutions/f6030c22-b53e-4d9f-8c9e-a4aa110c412e/solution.sql
@@ -1,2 +1,4 @@
-SELECT DISTINCT JOB_ID FROM employees
-order by job_id;
+select first_name, last_name, department_id, salary * 1.2 as "salario incrementado"
+from employees
+where department_id = 30 and salary * 1.2 < 4000
+order by salary;
